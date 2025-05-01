b46ffbdf34234d40fa6dd60d0c1d51096c440e51
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 17:53:13 -0500

    ✨ feat(auth): Implementa verificación de token y autenticación basada en cookies
    - Mejora el endpoint de login para gestionar cookies en la respuesta.
    - Añade un nuevo endpoint `verify` para la validación del token.
    - Implementa un flujo de autenticación basado en cookies utilizando `auth_token`.
    - Refuerza la seguridad al manejar la ausencia de token con respuesta 401 (Unauthorized).

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,11 +6,14 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { LoginAuthDto } from './dto/login-auth-dto';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -22,8 +25,21 @@ export class AuthController {
   }
 
   @Post('login')
-  login(@Body() loginAuthDto: LoginAuthDto) {
-    return this.authService.login(loginAuthDto);
+  async login(@Body() loginAuthDto: LoginAuthDto, @Res() res: Response) {
+    return this.authService.login(loginAuthDto, res);
+  }
+
+  @Get('verify')
+  async verify(@Req() req: Request, @Res() res: Response) {
+    const token: string | undefined = req.cookies['auth_token'] as
+      | string
+      | undefined;
+
+    if (!token) {
+      return res.status(401).json({ message: 'No hay ningun token' });
+    }
+
+    return this.authService.verifyToken(token, res);
   }
 
   @Get()
