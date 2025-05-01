87c16cc89e452958f85991e488c56536cb2d4d74
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 17:44:57 -0500

    ✏️ fix(auth): cambia validacion en password
    se cambia la validacion porque se tipaba como si fuera para correp pero era para una contraseña

--- src/auth/dto/login-auth-dto.ts
+++ src/auth/dto/login-auth-dto.ts
@@ -1,11 +1,11 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginAuthDto {
   @IsEmail({}, { message: 'El correo electrónico no es válido' })
   @IsString({ message: 'El correo electrónico debe ser una cadena de texto' })
   email: string;
 
-  @IsEmail({}, { message: 'La contraseña no es válida' })
+  @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
   @IsString({ message: 'La contraseña debe ser una cadena de texto' })
   password: string;
 }
