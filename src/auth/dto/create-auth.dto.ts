dbdc3339478c26f75e3a3b2fb63ffd9f5532a935
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 16:09:01 -0500

    ✨ feat(auth): añadir reglas de validación a los DTOs de autenticación
    - Aplica decoradores de `class-validator` para validar estructura y contenido de los datos de entrada.
    
    - Valida el formato del correo electrónico, el tipo de dato como cadena y la obligatoriedad de los campos requeridos.
    
    - Incluye mensajes de error personalizados en español para una mejor experiencia del usuario.
    
    - Refuerza la calidad e integridad de los datos en los flujos de autenticación.

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,5 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateAuthDto {
+  @IsString({ message: 'El nombre debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
   name: string;
+
+  @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  @IsString({ message: 'El correo electrónico debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El correo electrónico no puede estar vacío' })
   email: string;
+
+  @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+  @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
   password: string;
 }
--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAuthDto } from './create-auth.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {
+  @IsString({
+    message: 'El nombre debe ser una cadena de texto',
+  })
+  @IsNotEmpty({
+    message: 'El nombre no puede estar vacío',
+  })
+  name?: string;
+
+  @IsString({
+    message: 'El correo electrónico debe ser una cadena de texto',
+  })
+  @IsNotEmpty({
+    message: 'El correo electrónico no puede estar vacío',
+  })
+  email?: string;
+}
