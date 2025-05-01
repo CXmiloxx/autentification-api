809f812ae3592023a5a9ac5f9223d111a6068ebb
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 17:43:39 -0500

    ✨ feat(core): agrega cookie-parser y habilita CORS en la configuración de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,7 +23,12 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap().catch((error) => {
   console.error('Failed to start application:', error);
