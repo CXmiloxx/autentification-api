b86c12d8a11d34cc2c95c6d3005d99019d48af85
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 17:51:09 -0500

    ✨ feat(auth): crea autenticación JWT con manejo seguro de cookies
    - Añadido almacenamiento seguro de tokens de autenticación en cookies httpOnly.
    - Integrada la verificación de tokens mediante un endpoint dedicado.
    - Mejorado el flujo de inicio de sesión con generación de JWT.
    - Configurados parámetros de seguridad para las cookies (secure, httpOnly, sameSite).
    - Asegurado que todos los tokens se manejen del lado del servidor con medidas de seguridad adecuadas.
    
    Ahora, los tokens de autenticación están correctamente gestionados con cookies seguras y la validación de JWT se realiza de manera eficiente y segura.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,23 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ResponseAuthDto } from './dto/response-auth.dto';
 import { LoginAuthDto } from './dto/login-auth-dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async create(createAuthDto: CreateAuthDto) {
     try {
@@ -60,8 +66,10 @@ export class AuthService {
       );
     }
   }
-
-  async login(loginAuthDto: LoginAuthDto) {
+  async login(
+    loginAuthDto: LoginAuthDto,
+    @Res() res: Response,
+  ): Promise<Response> {
     try {
       const userAuth = await this.prisma.user.findUnique({
         where: { email: loginAuthDto.email },
@@ -83,14 +91,52 @@ export class AuthService {
         );
       }
 
-      return {
+      const payload = { id: userAuth.id };
+
+      const token = this.jwtService.sign(payload);
+
+      res.cookie('auth_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+        maxAge: 36000 * 1000,
+      });
+
+      return res.status(HttpStatus.OK).json({
         message: 'Inicio de sesión exitoso',
-      };
+      });
     } catch (error) {
       throw new HttpException(
         error instanceof Error ? error.message : 'Error de inicio de sesión',
-        HttpStatus.INTERNAL_SERVER_ERROR, // Código 500 para error del servidor
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
+    }
+  }
+
+  async verifyToken(token: string, res: Response) {
+    try {
+      const decoded = this.jwtService.verify<ResponseAuthDto>(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      const user = await this.prisma.user.findUnique({
+        where: { id: decoded.id },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+
+      if (!user) {
+        throw new HttpException('Usuario no encontrado', HttpStatus.NOT_FOUND);
+      }
+
+      return res.status(200).json(user);
+    } catch (error) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        message: error instanceof Error ? error.message : 'Token inválido',
+      });
     }
   }
 
