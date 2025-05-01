2cd31ea15ff1562362b52ef4ef09c409f1003d71
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-05-01 16:11:44 -0500

    ✨ feat(auth): agrega endpoint de inicio de sesión y ajustar rutas en el controlador de autenticación
    - Añade el endpoint `POST /auth/login` para manejar la autenticación de usuarios mediante credenciales válidas.
    - Renombra el endpoint existente `POST /auth` a `POST /auth/create` para una semántica más clara en el registro de usuarios.
    - Importa y utiliza `LoginAuthDto` para validar y estructurar las credenciales de acceso.
    - Mejora la organización de las rutas y la legibilidad del controlador de autenticación.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginAuthDto } from './dto/login-auth-dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('create')
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
+  }
+
+  @Post('login')
+  login(@Body() loginAuthDto: LoginAuthDto) {
+    return this.authService.login(loginAuthDto);
   }
 
   @Get()
